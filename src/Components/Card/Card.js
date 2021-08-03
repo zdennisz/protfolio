052748f400270f655ea4ac8964c06a7a45635e4e
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2021-08-03 15:02:04 +0300

    Fixed the key issue

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,49 +2,58 @@ import React from 'react';
 import './Card.css'
 
 
+const DescriptionItem = (props) => {
+    const { val } = props
+    return (<li>{val}</li>)
+}
+
+const ButtonItem = (props) => {
+    const { item } = props
+    let buttonClass = 'basic_button';
+    let link;
+    let buttonContent
+    if (item.hasOwnProperty('github')) {
+        buttonClass += ' github_button';
+        link = item.github;
+        buttonContent = "Github";
+    } else if (item.hasOwnProperty('website')) {
+        buttonClass += ' website_button';
+        link = item.website;
+        buttonContent = "Website";
+    } else if (item.hasOwnProperty('googleplay')) {
+        buttonClass += ' googleplay_button'
+        link = item.googleplay;
+        buttonContent = "Google Play";
+    }
+
+    if (link === "") {
+        return (<></>)
+    } else {
+        return (<li className="li_single_item"><a href={link} className={buttonClass}>{buttonContent}</a></li>)
+    }
+}
+
 const Card = (props) => {
 
-    const data = props.card
-    const listOfProps = data.description.map((item, index) => {
-
-        return (<li key={index.toString()}>{item}</li>)
+    const { card } = props
+    const listOfContentDescription = card.description.map((item, index) => {
+        return <DescriptionItem key={index.toString()} val={item} />
     })
 
-    const listOfButtons = data.buttons.map((item, index) => {
+    const listOfButtons = card.buttons.map((item, index) => {
 
-        let buttonClass = 'basic_button';
-        let link;
-        let buttonContent
-        if (item.hasOwnProperty('github')) {
-            buttonClass += ' github_button';
-            link = item.github;
-            buttonContent = "Github";
-        } else if (item.hasOwnProperty('website')) {
-            buttonClass += ' website_button';
-            link = item.website;
-            buttonContent = "Website";
-        } else if (item.hasOwnProperty('googleplay')) {
-            buttonClass += ' googleplay_button'
-            link = item.googleplay;
-            buttonContent = "Google Play";
-        }
-
-        if (link === "") {
-            return (<></>)
-        } else {
-            return (<a key={index.toString()} href={link} className={buttonClass}>{buttonContent}</a>)
-        }
+        return <ButtonItem key={index.toString()} item={item} />
 
     })
 
     return (
         <div className="card_container">
             <div className="image_container">
-                <img className="image" src={data.image} alt="Image of Card" />
+                <img className="image" src={card.image} alt="Card" />
             </div>
-            <div className="title_top">{data.title}</div>
-            <ul className="card_content">{listOfProps}</ul>
-            <div className="links">{listOfButtons}</div>
+            <div className="title_top">{card.title}</div>
+            <ul className="card_content">{listOfContentDescription}</ul>
+            <ul className="links">{listOfButtons}</ul>
         </div>
     )
 
